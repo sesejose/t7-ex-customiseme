bb16fd2648fd5f6175280a98bec6c5394eb6571c
Jose <sesejose@gmail.com>
2022-03-07 14:27:22 +0100

    First changes by Jose

---
+++ functions.js
@@ -0,0 +1,8 @@
+function openmenumobile() {
+  var x = document.getElementById("mainmenu");
+  if (x.style.display !== "none") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "inline-grid";
+  }
+}
