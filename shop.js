ea8fe2f4ae7cc838f51f1eaad4a9cc788aa591b6
Jose <sesejose@gmail.com>
2022-03-08 14:20:12 +0100

    create-soap page and css

---
+++ shop.js
@@ -0,0 +1,35 @@
+const url = "https://customizeme-d28a.restdb.io/rest/customizeme";
+
+const options = {
+    headers: {
+        "x-apikey": "62262a57dced170e8c83a001",
+    },
+};
+fetch(url, options)
+    .then((response) => {
+        if (!response.ok) {}
+        return response.json();
+    })
+    .then((data) => {
+        handlesoapList(data);
+    });
+
+function handlesoapList(data) {
+    data.forEach(showSoap);
+}
+
+function showSoap(soap) {
+    console.log(soap);
+    const template = document.querySelector("#cartTemplate").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector(".soap-name").textContent = soap.name;
+    copy.querySelector(".soap-image").src = soap.image;
+    copy.querySelector(".soap-price").textContent = soap.price + " " + "DDK";
+    copy.querySelector(
+        ".soap-image"
+    ).src = https://customizeme-d28a.restdb/image/622629a48d779a0100061a98.png;
+    /*     copy.querySelector("a").setAttribute("href", product.html?id=${soap._id});
+     */
+    const parent = document.querySelector("main");
+    parent.appendChild(copy);
+}
